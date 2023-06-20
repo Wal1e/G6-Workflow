60f16972f5d52ed7fb0f77262af9ead91d157a12
wangshangzhe <987261988@qq.com>
2023-06-20 16:39:21 +0800

    project flow

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Workflow from './components/Workflow.vue'
+import ProjectFlow from './components/ProjectFlow.vue';
+</script>
+
+<template>
+  <!-- <Workflow msg="Vite + Vue" /> -->
+  <ProjectFlow msg="项目流程图"></ProjectFlow>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Workflow.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+// import { ref } from 'vue'
+ // 扩展了节点、边的G6
+import G6 from '@antv/g6'
+// import * as G6Util from '@antv/util'
+import { TREELIST } from '../data/treeList';
+const props = defineProps<{ msg: string }>()
+const initData = {
+  // 点集
+  nodes: [
+    {
+      id: 'node1', // 节点的唯一标识
+      type: 'rect',
+      size: [100, 40],
+      x: 100, // 节点横坐标
+      y: 200, // 节点纵坐标
+      label: '起始点', // 节点文本
+    },
+    {
+      id: 'node2',
+      type: 'rect',
+      size: [100, 40],
+      x: 300,
+      y: 400,
+      label: '目标点',
+    },
+  ],
+  // 边集
+  edges: [
+    // 表示一条从 node1 节点连接到 node2 节点的边
+    {
+      source: 'node1', // 起始点 id
+      target: 'node2', // 目标点 id
+      label: '我是连线', // 边的文本
+      type: 'cubic'
+    },
+  ],
+};
+onMounted(() => {
+  console.log('mounted')
+  const data = {
+  nodes: [
+    {
+        id: '11',
+      x: 100,
+      y: 100,
+      type: 'rect',
+      label: 'rect',
+    },
+  ],
+};
+
+const graph = new G6.Graph({
+  container: 'mountNode',
+  width: 500,
+  height: 300,
+  defaultNode: {
+    type: 'rect', // 在数据中已经指定了 type，这里无需再次指定
+    style: {
+      fill: '#bae637',
+      stroke: '#eaff8f',
+      lineWidth: 5,
+      radius: 10,
+    },
+  },
+});
+graph.data(data);
+graph.render();
+})
+</script>
+
+<template>
+  <div id="mountNode"></div>
+</template>
+
+<style scoped>
+
+</style>
